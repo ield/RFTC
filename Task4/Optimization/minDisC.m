738c918beb18b641841c39cf6b3f934cf9eb281f
ield <nachol2013@gmail.com>
2020-11-13 16:52:27 +0100

    Added script to make approximation of L and C

--- Task4/Optimization/minDisC.m
+++ Task4/Optimization/minDisC.m
@@ -1,7 +1,8 @@
 function dis = minDisC(C, f, Y_im)
 % Calculates the distance between the curve of the admittance C and Y in
 % the frequencies f
+C = C*1e-12;
 Yc_im = 2*pi*f*C;
-dis = sum((Yc_im-Y_im).^2);
+dis = Yc_im-Y_im;
 end
 
--- Task4/Optimization/minDisLC.m
+++ Task4/Optimization/minDisLC.m
@@ -1,9 +1,9 @@
 function dis = minDisLC(LC, f, Y_im)
 % Calculates the distance between the curve of the admittance LC and Y in
 % the frequencies f
-L = LC(1);
-C = LC(2);
+L = LC(1)*1e-9; % It is important to normalize
+C = LC(2)*1e-12;
 Ylc_im = 2*pi*f*C./(1-(2*pi*f).^2*L*C);
-dis = sum((Ylc_im-Y_im).^2);
+dis = Ylc_im-Y_im;
 end
 
